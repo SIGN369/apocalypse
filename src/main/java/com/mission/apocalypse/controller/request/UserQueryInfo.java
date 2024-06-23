1120223e06ed78065c2e64726c295f5c4d855f58
yungui.zheng <yungui.zheng@nx-engine.com>
2024-06-23 13:08:03 +0800

    init

--- src/main/java/com/mission/apocalypse/controller/request/UserQueryInfo.java
+++ src/main/java/com/mission/apocalypse/controller/request/UserQueryInfo.java
@@ -1,12 +1,24 @@
 package com.mission.apocalypse.controller.request;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class UserQueryInfo {
 
+    @ApiModelProperty("姓名")
     private String name;
+
+    @ApiModelProperty("生日")
     private String birthday;
-    private String calendarType;
+
+    @ApiModelProperty("出生时辰")
+    private String birthHour;
+
+    @ApiModelProperty("公历 Calendar 农历 Lunar")
+    private String dateType;
+
+    @ApiModelProperty("性别 男Male 女:Female")
     private String gender;
 }
